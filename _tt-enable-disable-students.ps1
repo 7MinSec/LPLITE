8b3293bc6de5808316500d789f4edae70a78f2e7
Brian Johnson <17596787+7MinSec@users.noreply.github.com>
2023-05-22 14:52:17 -0500

    Create _tt-enable-disable-students.ps1

---
+++ _tt-enable-disable-students.ps1
@@ -0,0 +1,22 @@
+# A quick script to batch enable or disable students in LPLITE
+
+$confirmation = Read-Host "Do you need to DISABLE all students except student1?"
+if ($confirmation -eq 'y') {
+disable-adaccount student2
+disable-adaccount student3
+disable-adaccount student4
+disable-adaccount student5
+disable-adaccount student6
+disable-adaccount student7
+}
+
+$confirmation = Read-Host "Do you need to ENABLE all students?"
+if ($confirmation -eq 'y') {
+enable-adaccount student1
+enable-adaccount student2
+enable-adaccount student3
+enable-adaccount student4
+enable-adaccount student5
+enable-adaccount student6
+enable-adaccount student7
+}
